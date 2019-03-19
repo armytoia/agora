0091781ec09022115341c2b1f6445962a72609d6
armytoia <vitoriaacaldeirinha@gmail.com>
2019-03-19 16:13:54 -0300

    primeira_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  val1 : number = 0;
+  val2 : number = 0;
 
+  somar(): void{
+  let total = Number(this.val1) + Number(this.val2);
+  alert("a soma dos números é " +total);
+
+  if(total%2 == 0){
+    alert("par");
+  }
+  else{
+    alert("ímpar");
+  }
+ }
 }
